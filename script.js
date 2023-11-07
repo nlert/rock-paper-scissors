47366b3778b5e03cc822d82ffbc36e749a447f7a
nlert <neillertola@gmail.com>
2023-11-07 18:32:07 -0500

    Add responsive JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Player Choices and Event Listeners
 const rockButton = document.getElementById("rock-btn");
 const paperButton = document.getElementById("paper-btn");
 const scissorsButton = document.getElementById("scissors-btn");
@@ -5,49 +6,101 @@ const scissorsButton = document.getElementById("scissors-btn");
 rockButton.addEventListener('click', function() {
         let playerChoice = "rock";
         let computerChoice = getComputerChoice();
+
+        clearDisplay();
+
+        displayChoice("rock", computerChoice);
         playRound(playerChoice, computerChoice);
 });
 
 paperButton.addEventListener('click', function() {
     let playerChoice = "paper";
     let computerChoice = getComputerChoice();
+
+    clearDisplay();
+
+    displayChoice("paper", computerChoice);
     playRound(playerChoice, computerChoice);
 });
 
 scissorsButton.addEventListener('click', function() {
     let playerChoice = "scissors";
     let computerChoice = getComputerChoice();
+
+    clearDisplay();
+
+    displayChoice("scissors", computerChoice);
     playRound(playerChoice, computerChoice);
 });
 
-function playRound (playerChoice, computerChoice) {
+// Determine Winner
+const displayWinner = document.getElementById("score");
+
+function playRound(playerChoice, computerChoice) {
     if (
         (playerChoice == "rock" && computerChoice == "paper") ||
         (playerChoice == "paper" && computerChoice == "scissors") ||
         (playerChoice == "scissors" && computerChoice == "rock")
     ) {
-        console.log("You lose");
-        return "You lose";
+        displayWinner.textContent = "YOU LOSE";
     } else if (
         (playerChoice == "paper" && computerChoice == "rock") ||
         (playerChoice == "scissors" && computerChoice == "paper") ||
         (playerChoice == "rock" && computerChoice == "scissors")
     ) {
-        console.log("You win");
-        return "You win";
+        displayWinner.textContent = "YOU WIN";
     } else if (
         (playerChoice == "paper" && computerChoice == "paper") ||
         (playerChoice == "scissors" && computerChoice == "scissors") ||
         (playerChoice == "rock" && computerChoice == "rock")
     ) {
-        console.log("Tie");
-        return "Tie";
+        displayWinner.textContent = "TIE";
     };
 }
 
+// Display Player and Computer Selections
+const qMarkYou = document.getElementById("question-mark-you");
+const qMarkComp = document.getElementById("question-mark-comp");
+
+const playerReveal = document.getElementById("player-reveal");
+const compReveal = document.getElementById("comp-reveal");
+
+function displayChoice(playerChoice, computerChoice) {
+    qMarkYou.classList.add("hide");
+    qMarkComp.classList.add("hide");
+
+    if (playerChoice == "rock") {
+        playerReveal.classList.add("fa-hand-fist");
+    } else if (playerChoice == "paper") {
+        playerReveal.classList.add("fa-hand");
+    } else if (playerChoice == "scissors") {
+        playerReveal.classList.add("fa-hand-peace");
+    }
+
+    if (computerChoice == "rock") {
+        compReveal.classList.add("fa-hand-fist");
+    } else if (computerChoice == "paper") {
+        compReveal.classList.add("fa-hand");
+    } else if (computerChoice == "scissors") {
+        compReveal.classList.add("fa-hand-peace");
+    }
+}
+
+// Random Computer Selection
 function getComputerChoice() {
     arr = ["rock", "paper", "scissors"]
 
     let randomNum = Math.floor(Math.random() * arr.length);
     return arr[randomNum];
+}
+
+// Clear Selection Display Before Updating With New Selection
+function clearDisplay() {
+    playerReveal.classList.remove("fa-hand");
+    playerReveal.classList.remove("fa-hand-fist");
+    playerReveal.classList.remove("fa-hand-peace");
+
+    compReveal.classList.remove("fa-hand");
+    compReveal.classList.remove("fa-hand-fist");
+    compReveal.classList.remove("fa-hand-peace");
 }
